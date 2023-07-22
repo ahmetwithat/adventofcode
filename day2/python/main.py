f76a91ee524e2b2cb2b21f0c653ca3fd9edf2dcd
Ahmet Hakan <54170691+ahmetwithat@users.noreply.github.com>
2023-07-22 21:23:20 +0300

    Update main.py
    added some comments

--- day2/python/main.py
+++ day2/python/main.py
@@ -13,7 +13,7 @@ for i in range(len(data)):
   arr = data[i].split(' ')
   data[i] = arr
 
-for i in data:
+for i in data: #There really isn't any reason to do this here and not in the functions. 
   match i[0]:
     case 'A':
       i[0] = 1
@@ -22,7 +22,7 @@ for i in data:
     case 'C':
       i[0] = 3
 
-def DeterminePlay(elvesmove, outcome):
+def DeterminePlay(elvesmove, outcome): #this method also assingns numbers to rock-paper-scissors (like CalculateScore) and determines what the human should play to lose (x) draw (y) or win (z). i again, made up an algorithm to determine this because doing it with a bunch of if statements were boring -- And this works just fine! plus - i got to write an algorithm. how cool.
   match outcome:
     case 'X':
       play = (elvesmove+2)%3
@@ -36,7 +36,7 @@ def DeterminePlay(elvesmove, outcome):
         play = 3
   return play
 
-def CalculateScore(elvesmove, yourmove):
+def CalculateScore(elvesmove, yourmove): #this method basically assigns numbers to rock-paper-scissors and then using a simple algorithm, calculates who wins - and calculates the score according to the challenge. i did this because if statements are boring.
   outcomescore = 0
   match abs(elvesmove-yourmove):
     case 0:
